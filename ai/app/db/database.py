2c977fa088c56677db731355e39108f05dab361a
bys0223 <96911307+bys0223@users.noreply.github.com>
2025-11-10 15:52:36 +0900

    Update database.py

--- ai/app/db/database.py
+++ ai/app/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # SQLite 상대경로: ai/app/data/user.db
-SQLALCHEMY_DATABASE_URL = "sqlite:///./data/user.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./app/data/user.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
