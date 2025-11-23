d9a95b750c18a2dac9a12a367ba9fbd9cbe1e962
bys0223 <96911307+bys0223@users.noreply.github.com>
2025-11-23 16:44:30 +0900

    Update auth_service.py

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -28,7 +28,7 @@ def register_user(db: Session, email: str, name: str, password: str) -> models.U
 def authenticate_user(db: Session, email: str, name: str, password: str) -> Optional[models.User]:
     user = (
         db.query(models.User)
-        .filter(models.User.email == email, models.User.name == name)
+        .filter(models.User.email == email)
         .first()
     )
     if not user:
